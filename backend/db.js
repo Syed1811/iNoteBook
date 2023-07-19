a547b085e3e60f93edfce2bdc0b20037e9c22853
Syed980 <raiyanullah6@gmail.com>
2023-07-19 22:14:39 +0530

    iNoteBook

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 
+
 const connectToMongo = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/inotebook");
+    await mongoose.connect("mongodb://127.0.0.1:27017/inotebook");
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);

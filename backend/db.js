dc9937740c3ccb1901b4829588ca81edbbb90c7e
Syed Raiyanullah <100852456+Syed1811@users.noreply.github.com>
2023-07-20 11:55:33 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
-const connectToMongo = async () => {
+const connectToMongo =  () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/inotebook");
+     mongoose.connect("mongodb://127.0.0.1:27017/inotebook");
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
